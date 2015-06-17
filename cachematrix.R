101a17a3ade6b87a49d83e9c18bff7095a6b8c53
llagundalscoupe <djackan@hotmail.com>
2015-06-16 19:04:19 -0700

    My second updated version of cachematrix function with my additions

--- cachematrix.R
+++ cachematrix.R
@@ -1,45 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do        
+## Put comments here that give an overall description of what your functions do 
+
+        ## These functions create a special matrix like object that can cache it's inverse when available, and calculate the
+        ## inverse of the matrix if the matrix has not been cached.
+
 
 ## Write a short comment describing this function
 
-        ##This function creates a special matrix object that can cache it's inverse.
+        ## The makeCacheMatrix function creates a special matrix object that can cache it's inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         m<-NULL
-        ##sets the default values of the matrix in the makeCacheMatrix environment
-        set<-function(y) {
-                
+        
+        set<-function(y) {                
                 x<<-y
                 m<<-NULL
-        }
-        ##gets the values of the matrix
-        get<-function()x
-        ##sets the values of the inverse matrix
+        }   
+        get<-function()x 
         setinvm<-function(solve) m<<-solve
-        ##gets the values of the inverse matrix
         getinvm<-function() m
-        ##creates a list to store the above values
+        
         list(set=set,get=get,setinvm=setinvm, getinvm=getinvm)
 }
 
-
 ## Write a short comment describing this function
-        ##This function is used to create an inverse matrix, but first it checks to
-        ##to see if the inverse matrix is already in cache. If in cache it outputs a message and returns m, 
-        ##and if not in cache it proceeds to calculate the inverse matrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        
-        ##gets the cached value
+        ## The cacheSolve function is used to create an inverse matrix, but first it checks to
+        ## to see if the inverse matrix is already in cache. If the matrix exists in cache a message is printed and it returns m. 
+        ## If the matrix is not in cache it proceeds to calculate the inverse matrix, set the inverse matrix, and return m.
+
+cacheSolve <- function(x, ...) {      
         m<-x$getinvm() 
-        ##checks the cached value to see if it should be used or proceed to calculate the inverse of the input matrix
         if(!is.null(m)) {        
                 message("getting cached data")
                 return (m)
         }
-        ##If there was a NULL cached value the function proceeded and this calculates the inverse matrix, sets it, and then returns m
         data<-x$get()
         m<-solve(data,...)
         x$setinvm(m)
